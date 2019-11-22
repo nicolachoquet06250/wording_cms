60caf1b36593266803a2d14f3e86113e83ba8720
nicolas-choquet-2_admin <nicolas-choquet-2_admin@ssh4>
2019-11-22 15:18:02 +0100

    add first rout with params and add first service

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wording_cms
 cms multisite pour un wording dynamique
+
+#use Slim PHP Framework
+SITE => http://www.slimframework.com
--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
+	use DI\Container;
 	use Psr\Http\Message\ResponseInterface as Response;
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Slim\Factory\AppFactory;
 
 	require __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__.'/src/autoload.php';
+
+	echo '<pre>';
+
+	$container = new Container();
+	$container->set('cookies', function() {
+		return new \services\Cookies();
+	});
+	AppFactory::setContainer($container);
 
 	$app = AppFactory::create();
 
-	$app->get('/', function (Request $request, Response $response, $args) {
-		$response->getBody()->write("Hello world!");
+	$app->addRoutingMiddleware();
+
+	$app->get('/hello/{name}', function ($request, $response, $args) use($app) {
+		$response->getBody()->write("Hello ".$args['name']."<br/>");
+
+		$routeParser = $app->getRouteCollector()->getRouteParser();
+		$response->getBody()->write($routeParser->urlFor('hello', ['name' => 'Nicolas']));
+
+		$this->get('cookies')->set('name');
+
 		return $response;
+	})->setName('hello');
+
+        $app->get('/', function (Request $req, Response $res, $args) {
+		$res->getBody()->write("Hello world!");
+		return $res;
 	});
 
 	$app->run();
