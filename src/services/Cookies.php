e818669b047eb1ca8e759de2b167e16eb6728e86
nicolas-choquet-2_admin <nicolas-choquet-2_admin@ssh4>
2019-11-22 15:14:17 +0100

    add first rout with params and add first service

---
+++ src/services/Cookies.php
@@ -0,0 +1,8 @@
+<?php
+	namespace services;
+
+	class Cookies {
+		public function set($name) {
+			var_dump('set cookie with name '.$name);
+		}
+	}
---
+++ src/services/autoload.php
@@ -0,0 +1,3 @@
+<?php
+
+	require_once __DIR__.'/Cookies.php';
