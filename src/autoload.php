c1c8d3aef24f234bce0b7e81e438263b4b73e31f
Choquet Nicolas <nicolachoquet06250@gmail.com>
2019-12-20 13:50:28 +0100

    reset repo with slim-skeleton

--- src/autoload.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-	require_once __DIR__.'/services/autoload.php';
--- src/services/Cookies.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace services;
-
-	class Cookies {
-		public function set($name) {
-			var_dump('set cookie with name '.$name);
-		}
-	}
